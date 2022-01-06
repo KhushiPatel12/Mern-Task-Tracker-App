7064b10e3f0882b63279702a56657e493d32e1a9
KhushiPatel12 <92209863+KhushiPatel12@users.noreply.github.com>
2022-01-07 00:27:29 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Mern-Task-Tracker-App
+# MERN-Task-Tracker-App
+
+This Task Tracker App is fullstack App
+
+REQUIREMENT
+- Frontend (React)
+- Backend (Node - Express)
+- Database (MongoDB) (local)
+
+FRONTEND
+- Register / Login
+- List all existing tasks
+- Add / Update / Remove a Task
+- Toggle Reminder on a task
+
+BACKEND & DATABASE
+- API Creation and database according to the need
---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const mongoose = require("mongoose")
+const task = require("./model/taskModel")
+const User = require("./model/userModel")
+const tsk = require("./Router/taskRouter")
+const us = require("./Router/userRouter")
+app.use(express.json())
+const port = 4000
+
+mongoose.connect("mongodb://127.0.0.1:27017/tasktrackerapp").then(()=>{console.log("connected ...")})
+app.use("/task",tsk)
+app.use("/userpage",us)
+app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
